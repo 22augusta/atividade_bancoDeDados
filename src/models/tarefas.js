3ae17887767ecc16eec547df314cb256de1c050b
22augusta <aguiar.maria22@gmail.com>
2021-12-06 21:21:39 -0300

    Nova API

---
+++ src/models/tarefas.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const tarefasSchema = new mongoose.Schema({
+    id: {type : Number},
+    descricao: { type : String },
+    dataInclusao: {type : String},
+    concluido: {type : Boolean},
+    nomeColaborador: {type : String}
+}, {
+    versionKey: false
+});
+
+const tarefas = mongoose.model('tarefas', tarefasSchema);
+
+module.exports = tarefas; 
